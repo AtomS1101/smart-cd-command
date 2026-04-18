b715a770a243a6763b4b0f2e5c71f55bc0a2550f
Atom <121838254+AtomS1101@users.noreply.github.com>
2026-04-18 13:15:53 +0900

    Create fcd.sh

---
+++ fcd.sh
@@ -0,0 +1,38 @@
+fcd () {
+	local search_path="${FCD_PATH:-$HOME/Documents}" #Change to the directory you want to search
+	local query="$1" 
+	local flag="$2" 
+	if [[ -z "$query" ]]
+	then
+		echo "Usage: fcd <query> [-e]"
+		return 1
+	fi
+	local exact_matches=() 
+	exact_matches=(${(f)"$(find "$search_path" -type d -iname "$query" 2>/dev/null)"}) 
+	local matches=() 
+	if [[ ${#exact_matches[@]} -gt 0 ]]
+	then
+		matches=("${exact_matches[@]}") 
+	else
+		matches=(${(f)"$(find "$search_path" -type d -iname "*${query}*" 2>/dev/null)"}) 
+	fi
+	if [[ ${#matches[@]} -eq 0 ]]
+	then
+		echo "fcd: no such directory: $query"
+		return 1
+	elif [[ ${#matches[@]} -eq 1 ]]
+	then
+		if [[ "$flag" == "-e" ]]
+		then
+			echo "${matches[1]}"
+		else
+			cd "${matches[1]}" || return 1
+		fi
+	else
+		echo "Multiple matches found:"
+		for i in {1..${#matches[@]}}
+		do
+			echo "  $((i-1)): ${matches[$i]}"
+		done
+	fi
+}
